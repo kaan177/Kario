6b880463f6ff044399b03ef600e5f7a3b2631893
kaan177 <66557991+kaan177@users.noreply.github.com>
2024-11-09 23:11:13 +0100

    merge commit

--- src/Main.hs
+++ src/Main.hs
@@ -21,32 +21,7 @@ main = do sprites <- loadImages
               input            -- Event function
               step             -- Step function
 
-loadImages :: IO Sprites
-loadImages = do kario <- loadBMP "assets\\Kario.bmp"
-                ground <- loadBMP "assets\\Ground.bmp"
-                brick <- loadBMP "assets\\Brick.bmp"
-                questionMark <- loadBMP "assets\\QuestionMarkBlock.bmp"
-                brokenQuestionMark <- loadBMP "assets\\BrokenQuestionMarkBlock.bmp"
-                coin <- loadBMP "assets\\Coin.bmp"
-                coinBling <- loadBMP "assets\\CoinBling.bmp"
-                menu <- loadBMP "assets\\StartScreen.bmp"
-                levelBox <- loadBMP "assets\\LevelBox.bmp"
-                selectionRing <- loadBMP "assets\\SelectionRing.bmp"
-                koomba <- loadBMP "assets\\Koomba.bmp"
-                flagPole <- loadBMP "assets\\FlagPole.bmp"
-                return Sprites {
-                    karioImage = kario,
-                    groundImage = ground,
-                    brickImage = brick,
-                    questionMarkImage = questionMark,
-                    brokenQuestionMarkImage = brokenQuestionMark,
-                    coinPictures =  [coin, coinBling],
-                    menuImage = menu,
-                    levelBoxImage = levelBox,
-                    selectionRingImage = selectionRing,
-                    koombaImage = koomba,
-                    flagPoleImage = flagPole
-                    }
+
 
 loadLevels :: IO [String]
 loadLevels = do
